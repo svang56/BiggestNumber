c259db29e1cca46621a73e8516584deef593a3e8
svang56 <svang56@mail.csuchico.edu>
2018-02-05 17:16:31 -0800

    Insertion of whole numbers

--- biggestnumber.cpp
+++ biggestnumber.cpp
@@ -1,14 +1,17 @@
-//Author:
+//Author:Sunny Vang
 #include<iostream>
 
 using namespace std; //so we don't need the prefix std::
 
 int main()
 {
-  
+ int numbone;
+ int numbtwo;
   cout<<"Please enter a whole number:\n";
+  cin>> numbone;
   
   cout<<"Please enter another whole number:\n";
+  cin>> numbtwo;
 
   cout<<"Of those two numbers, the biggest is: ";
   cout<<endl<<"Thank you for playing.\n";
