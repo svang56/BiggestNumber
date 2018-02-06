6ade35f08155aaa848e6e28988b1f451018f43fc
svang56 <svang56@mail.csuchico.edu>
2018-02-05 17:46:02 -0800

    Finished Number comparing program

--- biggestnumber.cpp
+++ biggestnumber.cpp
@@ -7,13 +7,29 @@ int main()
 {
  int numbone;
  int numbtwo;
+ int biggest;
+
   cout<<"Please enter a whole number:\n";
   cin>> numbone;
   
   cout<<"Please enter another whole number:\n";
   cin>> numbtwo;
+if(numbone > numbtwo)
+{
 
-  cout<<"Of those two numbers, the biggest is: ";
+biggest = numbone;
+
+
+}
+else
+
+{
+
+biggest = numbtwo;
+
+
+}
+  cout<<"Of those two numbers, the biggest is: "<< biggest;
   cout<<endl<<"Thank you for playing.\n";
 
   return 0;
